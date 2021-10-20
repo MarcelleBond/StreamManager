da1bc7be9a6739b014d00c625b52171fb0ba023f
Marcelle <marcelleb@bbd.co.za>
2021-10-20 02:26:17 +0200

    receiving data and opening tabs

--- background.js
+++ background.js
@@ -1,36 +1,80 @@
-const url = "localhost:5000/api/stream/getuser"
+const getUser = `https://localhost:5001/api/test/getUsers`
+const getSongs = `https://localhost:5001/api/test/getSongs`
 const userdata = {
-	username,
-	password,
-	playList: []
+	username: "",
+	password: "",
+	playList: [{ url: "", time: "", played: false }],
+	valid: false
 }
+
+
 /*
 * get username and password
 *		# make http request to the server to collect the username and password for one account
-*		# read values from document for now
+*		# store username and password in local storage
 */
 
 chrome.runtime.onInstalled.addListener(() => {
-
+	getUsername()
 })
 
-
-
-function setup(params) {
-	fetch(url).then((resp) => {
-
-		resp.body.
-		chrome.storage.sync.set(userdata)
-	}).catch((err) => {
-
+function setupStreams() {
+	userdata.playList.forEach(el =>{
+		if (el.played == false)
+		{
+			setTimeout(() => {
+				chrome.tabs.create({
+					url: el.url
+				})
+				el.played = true
+			}, el.time)
+		}
 	})
 }
 
+function getUsername(params) {
 
+	let noCache = true
 
-/*
-* store username and password
-*/
+	chrome.storage.sync.get("userdata", ({ dockeruser }) => {
+
+		if (dockeruser) {
+
+			Object.assign(userdata, dockeruser)
+			console.log(userdata.valid)
+			noCache = false
+		}
+	})
+
+	if (noCache) {
+
+		fetch(getUser).then(response => response.json())
+			.then(data => {
+				console.log("response from get user",data)
+				Object.assign(userdata, data)
+				chrome.storage.sync.set({ dockeruser: userdata })
+				console.log("Userdata after getting user name and password",userdata)
+				getSongList()
+			}).catch((err) => {
+
+				console.log(err)
+			})
+	}
+}
+
+function getSongList() {
+	fetch(getSongs).then(response => response.json())
+		.then(data => {
+			console.log("response from get songs",data)
+			Object.assign(userdata, data)
+			chrome.storage.sync.set({ dockeruser: userdata })
+			console.log("Userdata after getting somgs: ",userdata)
+			setupStreams()
+		}).catch((err) => {
+
+			console.log(err)
+		})
+}
 
 // pick random times to play song on url
 
